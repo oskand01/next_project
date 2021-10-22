ee017719466052ed18f07764ee8b0ee8d8176f6e
Oskar Andersson <oskand01@gmail.com>
2021-10-22 21:57:38 +0200

    add header component

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <h1>Buy a human</h1>
     </div>
-  )
+  );
 }
