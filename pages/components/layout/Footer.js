5375b64ec738937c90dd63a419edcefdd93417ae
saurabh311 <saurabh311@gmail.com>
2021-10-22 15:22:23 +0200

    create footer and layout component

---
+++ pages/components/layout/Footer.js
@@ -0,0 +1,9 @@
+//import styles from '.../styles/Home.module.css'
+
+export default function Footer (){
+    return(
+        <nav style={{width: "100%", display: "flex", flexDirection: "column", background:"black", color:"white", textAlign:"center"}}>
+         <p>This page was made with next.js using packages</p>
+        </nav>
+    );
+}
---
+++ pages/components/layout/index.js
@@ -0,0 +1,13 @@
+
+import Footer from "./Footer";
+
+export default function Layout ({children}){
+    return(
+        <div>
+           
+           {children}
+           <Footer/>
+        </div>
+    );
+
+}
