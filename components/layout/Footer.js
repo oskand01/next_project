661c63f6e70d0cbe6b8c4627fb3774d2e6929361
saurabh311 <saurabh311@gmail.com>
2021-10-24 23:05:12 +0200

    added footer

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -1,8 +1,8 @@
-import styles from '../../../styles/Home.module.css'
+import styles from "../../styles/Footer.module.css";
 
 export default function Footer (){
     return(
-        <div className={styles.Footer}>        
+        <div className={styles.footer}>        
          <p>This page was made with next.js using packages</p>
         </div>
     );
