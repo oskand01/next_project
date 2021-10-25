1017bc69bb2f4449a20db32882e55d45b72f81ad
Oskar Andersson <oskand01@gmail.com>
2021-10-25 20:58:46 +0200

    order by img first

--- pages/index.js
+++ pages/index.js
@@ -43,11 +43,15 @@ export async function getStaticProps() {
     Object.assign(data[i], { id: i });
     +Object.assign(data[i], { price: generatePrice() });
     if (data[i].image === "") {
-      data[i].image = "/../public/avatar.jpg"
+      data[i].image = "/../public/avatar.jpg";
     }
   }
-  const shuffledData = arrayShuffle(data);
-  return { props: { charProducts: shuffledData } };
+  const withPic = data.filter((item) => item.image !== "/../public/avatar.jpg");
+  const noPic = data.filter((item) => item.image === "/../public/avatar.jpg");
+  const shuffledWithPic = arrayShuffle(withPic);
+  const shuffledNoPic = arrayShuffle(noPic);
+
+  return { props: { charProducts: [...shuffledWithPic, ...shuffledNoPic] } };
 }
 
 function generatePrice() {
